8b4058f22d8ab6aad6e07df62d2cf38ca65163d4
arturm22 <31637800+arturm22@users.noreply.github.com>
2023-06-28 16:57:30 -0300

    Create pre_processamento.cpp

---
+++ Codigos/pre_processamento.cpp
@@ -0,0 +1,36 @@
+#include <opencv2/opencv.hpp>
+
+void pre_processamento(){
+    
+    cv::Mat imagem_placa = cv::imread("..\\Imagem\\Imagem_temporaria.jpg"); 
+
+    if (imagem_placa.empty())
+        return;
+    
+    // Aumentar a imagem para um melhor ajuste no tesseract
+
+    cv::Mat img_aumentada;
+    cv::resize(imagem_placa, img_aumentada, cv::Size(), 4, 4, cv::INTER_CUBIC);
+    
+    // O 50 e o 255 são colocados na placa, é a margem do quanto de branco e cinza serão adicionados na imagem para realçar as letras 
+
+    cv::Mat img_cinza;
+    cv::cvtColor(img_aumentada, img_cinza, cv::COLOR_BGR2GRAY);
+    
+    //img_cinza = cv2.cvtColor(img_aumentada, cv2.COLOR_BGR2GRAY)
+
+    cv::Mat img_binaria;
+    cv::threshold(img_cinza, img_binaria, 65, 200, cv::THRESH_BINARY);
+
+    // Salvar a imagem binarizada e em cinza
+
+    cv::imwrite("../Imagem/placa_binarizada.jpg", img_binaria);
+    cv::imwrite("../Imagem/placa_binarizada_png.png", img_binaria);
+
+    //cv2.imshow("ler_placa", imagem_placa)
+    //cv2.imshow("placa_cinza", img_cinza)
+    cv::imshow("placa_binaria", img_binaria);
+    cv::waitKey(0); 
+    cv::destroyAllWindows();
+
+}
