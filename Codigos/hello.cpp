4fdcc79bce9df8a134ad2dd9b2f0f051f0ba4bf3
arturm22 <31637800+arturm22@users.noreply.github.com>
2023-06-28 11:23:37 -0300

    MakeFile

---
+++ Codigos/hello.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <opencv2/opencv.hpp>
+using namespace cv;
+
+int main(int argc, char** argv)
+{
+	if(argc != 2)
+	{
+		printf("Modo de uso: %s Nome_Imagem\n",
+			argv[0]);
+		return -1;
+	}
+	// Ler imagem e guardar em um objeto
+	// da classe 'Mat'
+	Mat image;
+	image = imread(argv[1]);
+	if (!image.data)
+	{
+		printf("Imagem não-encontrada\n");
+		return -1;
+	}
+	// Criar janela
+	namedWindow(argv[1], WINDOW_AUTOSIZE);
+	// Mostrar imagem na janela
+	imshow(argv[1], image);
+	// Aguardar usuário pressionar
+	// algum botão
+	waitKey(0);
+	return 0;
+}
