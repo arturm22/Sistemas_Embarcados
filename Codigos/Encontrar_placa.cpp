16da4d9a3713f9cb71974304d2310c5d8b569584
arturm22 <31637800+arturm22@users.noreply.github.com>
2023-06-28 21:52:40 -0300

    Encontrar_placa.cpp

--- Codigos/Encontrar_placa.cpp
+++ Codigos/Encontrar_placa.cpp
@@ -1,5 +1,12 @@
+// g++ Encontrar_Placa.cpp -o Encontrar_Placa -Wall 'pkg-config --libs opencv4' (comandos para makefile)
+// Código para encontrar a placa funcionando 
 #include <opencv2/opencv.hpp>
+#include <vector>
+#include <iostream>
+#include <opencv2/core/mat.hpp>
+
   using namespace cv;
+  using namespace std;
   
   void encontrar_a_placa(String imagem_carro) {
     Mat img = imread(imagem_carro);
@@ -8,22 +15,22 @@
     cvtColor(img, cinza, COLOR_BGR2GRAY);
     
     Mat bin;
-    threshold(cinza, bin, 90, 255, THRESH_BINARY);
+    threshold(cinza, bin, 90, 230, THRESH_BINARY);
     
     Mat desfocar;
     GaussianBlur(bin, desfocar, Size(5,5), 0);
     imshow("Imagem_desfocada", desfocar);
     
-    vector<vector<Point> > contornos;
+    vector<vector<Point>> contornos;
     vector<Vec4i> hierarquia;
     findContours(desfocar, contornos, hierarquia, RETR_TREE, CHAIN_APPROX_NONE);
     
-    for (int i = 0; i < contornos.size(); i++) {
+    for (int i = 0; i < (int) contornos.size() ; i++) {
       double perimetro = arcLength(contornos[i], true);
       vector<Point> aprox;
       approxPolyDP(contornos[i], aprox, 0.03*perimetro, true);
       
-      if (perimetro > 120) {
+      if (perimetro > 300 && perimetro < 320) {
         Rect rect = boundingRect(contornos[i]);
         rectangle(img, rect, Scalar(0,255,0), 2);
         
@@ -35,4 +42,11 @@
     imshow("Placa_contornada", img);
     waitKey(0);
     destroyAllWindows();
+    
   }
+
+
+int main () {
+    encontrar_a_placa("../Imagem/carro4.jpg");
+    return 0;
+}
