913a568a0e9d8879999c4c5ab94ee7a88992c666
arturm22 <31637800+arturm22@users.noreply.github.com>
2023-07-05 19:30:42 -0300

    Update camerac2.cpp
    ALTERAÇÕES

--- Codigos/camerac2.cpp
+++ Codigos/camerac2.cpp
@@ -44,3 +44,35 @@ int main(int, char**)
  // the camera will be deinitialized automatically in VideoCapture destructor
  return 0;
 }
+
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <raspicam/raspicam_cv.h>
+
+using namespace cv;
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    Mat image, output;
+    raspicam::RaspiCam_Cv cap;
+    if (!cap.isOpened())
+    {
+        cout << "Could not initialize capturing...\n";
+        return 0;
+    }
+    int frameNumber = 0;
+    while (1)
+    {
+        cap.grab();
+        cap.retrieve(output);
+        imshow("webcam input", output);
+        string filename = "frame" + to_string(frameNumber) + ".jpg";
+        imwrite(filename, output);
+        frameNumber++;
+        char c = (char)waitKey(10);
+        if (c == 27)
+            break;
+    }
+}
